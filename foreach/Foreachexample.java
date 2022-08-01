83a6004d43cfa81353e0e5fe5dc28aade6c814fa
divyanethi123 <106815367+divyanethi123@users.noreply.github.com>
2022-08-01 11:25:42 +0530

    foreach examples

---
+++ foreach/Foreachexample.java
@@ -0,0 +1,17 @@
+package com.motivity.foreach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Foreachexample {
+    public static void main(String[] args) {  
+        List<String> gamesList = new ArrayList<String>();  
+        gamesList.add("Football");  
+        gamesList.add("Cricket");  
+        gamesList.add("Chess");  
+        gamesList.add("Hocky");  
+        System.out.println("------------Iterating by passing lambda expression--------------");  
+        gamesList.forEach(games -> System.out.println(games));  
+          
+    }  
+}
